51c60c0562e9050460369caafb15cf751ee5c645
qinghaitvxq <598616202@qq.com>
2017-06-28 15:27:40 +0800

    encapuslate d_table

--- js/directive/data_table.js
+++ js/directive/data_table.js
@@ -6,32 +6,24 @@
     app.directive("dTable",function () {
         return{
             restrict:"E",
-            template:"<div>sdfsdsfsdfs</div>",
+            template:"<table class='table table-striped'></table>",
+            replace:true,
             link:function (scope,element,attr) {
-                // <table class="table table-striped">-->
-                //     <!--<thead>-->
-                //     <!--<th>第一类别</th>-->
-                //     <!--<th>第二类别</th>-->
-                //     <!--<th>名称</th>-->
-                //     <!--<th>维护人</th>-->
-                //     <!--<th>更新时间</th>-->
-                //     <!--</thead>-->
-                //     <!--<tbody>-->
-                //     <!--<tr><td>hello</td>-->
+                //html
                 let dataTitle=["第一类别","第二类别","名称","维护人","更新时间"];
-                let dataList=[{
+                let dataList=[iteratorObj({
                     "tCategory":"hello1",
                     "sCategory":"hello2",
                     "name":"测试",
                     "mtname":"璐璐",
                     "utime":"2017-06-28"
-                },{
+                }),iteratorObj({
                     "tCategory":"test1",
                     "sCategory":"test2",
                     "name":"白白",
                     "mtname":"冬季",
                     "utime":"2017-06-29"
-                }];
+                })];
                 let headtemplate="",
                     thtemplate="";
                 
@@ -39,12 +31,38 @@
                     thtemplate+=`<th>${item}</th>`;
                 });
                 headtemplate=`<thead>${thtemplate}</thead>`;
-                
+
+                let trtemplate="",
+                    tbodytemplate="";
                 dataList.forEach(function (item) {
-                    
+                    let  tdtemplate="";
+                    for(let data of item){
+                        tdtemplate+=`<td>${data}</td>`;
+                    }
+                    trtemplate+=`<tr>${tdtemplate}</tr>`;
                 });
+                tbodytemplate=`<tbody>${trtemplate}</tbody>`;
+
+                $(element).append(headtemplate).append(tbodytemplate);
             }
         }
     });
+
+    function iteratorObj(obj) {
+        obj[Symbol.iterator]=function(){
+            let properties=Object.keys(obj);
+            let count=0;
+            let isDone=false;
+
+            let next=()=>{
+                if(count>=properties.length){
+                    isDone=true;
+                }
+                return {done:isDone,value:obj[properties[count++]]}
+            }
+            return {next};
+        }
+        return obj;
+    }
 
 })();
